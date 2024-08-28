9577bf92da9e58b77624af05185d8a4b49cdc86d
conao3 <conao3@gmail.com>
2024-08-28 17:17:28 +0900

    add spread symbol

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, ... }: {
     nixosConfigurations.perses = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
