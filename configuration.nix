7dc8dffec7d6121a90128fd9854465fe8abba2b1
conao3 <conao3@gmail.com>
2024-08-28 14:50:26 +0900

    add configurations

---
+++ configuration.nix
@@ -0,0 +1,111 @@
+{ config, lib, pkgs, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  system.stateVersion = "24.11";
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-order-than 7d";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  networking = {
+    firewall.enable = true;
+    hostName = "conao-nixos-perses";
+  };
+
+  time.timeZone = "Asia/Tokyo";
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5 = {
+        addons = with pkgs; [
+          fcitx5-skk
+        ];
+      };
+    };
+  };
+
+  services = {
+    # displayManager.defaultSession = "xfce";
+    displayManager.defaultSession = "none+i3";
+    xserver = {
+      enable = true;
+      desktopManager = {
+        xterm.enable = false;
+        xfce = {
+          enable = true;
+          enableScreensaver = false;
+        };
+      };
+      windowManager = {
+        i3 = {
+          enable = true;
+          extraPackages = with pkgs; [
+            dmenu
+            i3status
+            i3lock
+            i3blocks
+          ];
+        };
+      };
+    };
+    libinput = {
+      enable = true;
+      touchpad = {
+        disableWhileTyping = true;
+      };
+    };
+    openssh.enable = true;
+    spice-vdagentd.enable = true;
+  };
+
+  users.users.conao = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [
+      chromium
+      firefox
+      tree
+      git
+      tig
+      ghq
+      _1password-gui
+      # awscli2
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    emacs
+  ];
+
+  programs = {
+    fish.enable = true;
+    zsh.enable = true;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  virtualisation = {
+    docker.enable = true;
+  };
+}
