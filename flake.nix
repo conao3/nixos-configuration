aedb94d40c73d773320385cd0f53c00706088335
conao3 <conao3@gmail.com>
2024-08-29 09:40:30 +0900

    create home-manager folder

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.backupFileExtension = "backup";
-            home-manager.users.conao = import ./home.nix;
+            home-manager.users.conao = import ./home-manager/home.nix;
           }
         ];
       };
