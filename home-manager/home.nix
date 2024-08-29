4f66b5d5731ba3df5b24fd361999f3b21257db42
conao3 <conao3@gmail.com>
2024-08-29 10:04:20 +0900

    refactor

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -9,15 +9,17 @@
     ];
   };
 
-  programs.git = {
-    enable = true;
-    userName = "conao3";
-    userEmail = "conao3@gmail.com";
-  };
-  programs.zsh = {
-    enable = true;
+  programs = {
+    home-manager.enable = true;
+    git = {
+      enable = true;
+      userName = "conao3";
+      userEmail = "conao3@gmail.com";
+    };
+    zsh = {
+      enable = true;
+    };
   };
 
   home.stateVersion = "24.05";
-  programs.home-manager.enable = true;
 }
