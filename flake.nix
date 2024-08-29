724b538ff966dfbeabd2f59c71f023f369fcac58
conao3 <conao3@gmail.com>
2024-08-29 09:20:55 +0900

    add nixos folder

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       perses = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./configuration.nix
+          ./nixos/configuration.nix
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
