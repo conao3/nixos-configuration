f2c3100649c656474bfc0949d2d3e355b9388a8a
conao3 <conao3@gmail.com>
2024-08-29 15:18:49 +0900

    remove test

--- home-manager/nixpkgs/cljstyle.nix
+++ home-manager/nixpkgs/cljstyle.nix
@@ -1,11 +1,11 @@
-{ lib, buildGraalvmNativeImage, fetchurl, nix-update-script, testers, cljstyle }:
+{ lib, buildGraalvmNativeImage, fetchurl }:
 buildGraalvmNativeImage rec {
   pname = "cljstyle";
   version = "0.16.626";
 
   src = fetchurl {
     url = "https://github.com/greglook/${pname}/releases/download/${version}/${pname}-${version}.jar";
-    sha256 = "";
+    sha256 = "sha256-b6kWPFrYvoDu3g0ZSAw/L6rRxGfStBgkM1u9VZl6/x8=";
   };
 
   extraNativeImageBuildArgs = [
@@ -16,14 +16,6 @@ buildGraalvmNativeImage rec {
     "--report-unsupported-elements-at-runtime"
     "--no-fallback"
   ];
-
-  passthru.updateScript = nix-update-script {};
-
-  passthru.tests.version = testers.testVersion {
-    inherit version;
-    package = cljstyle;
-    command = "cljstyle --version";
-  };
 
   meta = with lib; {
     mainProgram = "cljstyle";
