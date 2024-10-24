d2d356281bc84b0d39a690ddbfba7674b27de63e
conao3 <conao3@gmail.com>
2024-10-24 13:48:03 +0900

    use nix to install font

--- home-manager/nix-darwin/default.nix
+++ home-manager/nix-darwin/default.nix
@@ -45,7 +45,6 @@
       "docker"
       "figma"
       "firefox"
-      "font-hackgen"
       "gimp"
       "google-japanese-ime"
       "jetbrains-toolbox"
@@ -59,4 +58,20 @@
       "xmind"
     ];
   };
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-lgc-plus
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-color-emoji
+    noto-fonts-emoji-blob-bin
+    noto-fonts-monochrome-emoji
+    hackgen-font
+    hackgen-nf-font
+    nerdfonts
+    emacs-all-the-icons-fonts
+    font-awesome
+    font-awesome_5
+  ];
 }
