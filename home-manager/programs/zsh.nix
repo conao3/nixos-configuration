eea439cb47a7fb332f3d779e3f9dea05b6b87d2a
conao3 <conao3@gmail.com>
2025-05-11 23:37:15 +0900

    use initContent instead of initExtra

--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -4,7 +4,7 @@
     eval "$(/opt/homebrew/bin/brew shellenv)"
     eval "$(anyenv init -)"
   '';
-  initExtra = ''
+  initContent = ''
     [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
   '';
   envExtra = ''
