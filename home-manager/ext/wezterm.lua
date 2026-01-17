594e794d846d294c902a934c1fca5a8fae438db5
conao3 <conao3@gmail.com>
2026-01-17 14:49:44 +0900

    fix wezterm config

--- home-manager/ext/wezterm.lua
+++ home-manager/ext/wezterm.lua
@@ -5,5 +5,4 @@ return {
     font = wezterm.font 'HackGen Console NF',
     font_size = 12,
     window_padding = { left = 1, right = 0, top = 0, bottom = 0 },
-    leader = { key = 'o', mods = 'CTRL', timeout_milliseconds = 1000 },
 }
