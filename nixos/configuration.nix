b630cc3c9a3034ba8a0314c3386982e36e57d5a3
conao3 <conao3@gmail.com>
2026-01-18 01:02:01 +0900

    add binary cache

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -24,6 +24,15 @@
         "root"
         "@wheel"
       ];
+      substituters = [
+        "http://binarycache.example.com"
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "binarycache.example.com-1:dsafdafDFW123fdasfa123124FADSAD"
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
   };
 
