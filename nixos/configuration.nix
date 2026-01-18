f7a5a46c0644b0918bbda578178324cf612b09c0
conao3 <conao3@gmail.com>
2026-01-18 12:25:02 +0900

    remove example substituters

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -25,12 +25,10 @@
         "@wheel"
       ];
       substituters = [
-        "http://binarycache.example.com"
         "https://nix-community.cachix.org"
         "https://cache.nixos.org/"
       ];
       trusted-public-keys = [
-        "binarycache.example.com-1:dsafdafDFW123fdasfa123124FADSAD"
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
