e38a2f902e7585f8cad5d638b28ef7e4bdced2e2
conao3 <conao3@gmail.com>
2026-01-18 13:41:26 +0900

    renew config

--- home-manager/ext/wezterm.lua
+++ home-manager/ext/wezterm.lua
@@ -1,8 +1,17 @@
 local wezterm = require('wezterm')
+local config = wezterm.config_builder()
 local act = wezterm.action
 
-return {
-    font = wezterm.font 'HackGen Console NF',
-    font_size = 12,
-    window_padding = { left = 1, right = 0, top = 0, bottom = 0 },
+config.font = wezterm.font 'HackGen Console NF'
+config.font_size = 12
+config.window_padding = { left = 1, right = 0, top = 0, bottom = 0 }
+config.window_decorations = "RESIZE"
+config.use_fancy_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.colors = {
+    tab_bar = {
+        background = "none"
+    }
 }
+
+return config
