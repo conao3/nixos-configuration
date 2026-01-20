3d211a40936beeaec512c098de761bdc65006b0b
conao3 <conao3@gmail.com>
2026-01-21 01:30:02 +0900

    add bindkey

--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -4,8 +4,9 @@
     which /opt/homebrew/bin/brew >/dev/null 2>&1 && eval "$(/opt/homebrew/bin/brew shellenv)"
     which anyenv >/dev/null 2>&1 && eval "$(anyenv init -)"
   '';
-  initContent = ''
+  initExtra = ''
     [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+    bindkey -e
   '';
   envExtra = ''
     [[ -s "$HOME/.cargo/env" ]] && . "$HOME/.cargo/env"
