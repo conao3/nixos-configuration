84f59be1602e5ab28aa9c6e269e4eaa00cc86244
conao3 <conao3@gmail.com>
2026-01-21 04:39:06 +0900

    use initContent

--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -4,7 +4,7 @@
     which /opt/homebrew/bin/brew >/dev/null 2>&1 && eval "$(/opt/homebrew/bin/brew shellenv)"
     which anyenv >/dev/null 2>&1 && eval "$(anyenv init -)"
   '';
-  initExtra = ''
+  initContent = ''
     [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
     bindkey -e
   '';
