4596e6ac6212db254f296f2fae6522046f0e3341
conao3 <conao3@gmail.com>
2026-01-20 13:50:52 +0900

    change keyboard settings

--- hosts/eos/default.nix
+++ hosts/eos/default.nix
@@ -11,4 +11,12 @@
   ];
 
   networking.hostName = "conao-nixos-eos";
+
+  services.xserver.xkb.options = "ctrl:nocaps";
+  console.useXkbConfig = true;
+
+  services.xserver = {
+    autoRepeatDelay = 300;
+    autoRepeatInterval = 30;
+  };
 }
