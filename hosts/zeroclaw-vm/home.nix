316db84234e162e9d68b2653166437bc3989a0d2
conao3 <conao3@gmail.com>
2026-02-21 11:05:21 +0900

    add emacs config

--- hosts/zeroclaw-vm/home.nix
+++ hosts/zeroclaw-vm/home.nix
@@ -7,7 +7,14 @@
 {
   home.stateVersion = "24.11";
 
+  home.file.".config" = {
+    source = ./ext/.config;
+    recursive = true;
+  };
+
   home.packages = [
     inputs.llm-agents.packages.${pkgs.system}.zeroclaw
+    pkgs.emacs
+    pkgs.wezterm
   ];
 }
