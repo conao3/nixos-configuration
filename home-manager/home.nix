18c68e20f1d886306278ab3b9f3ea9011100ae53
conao3 <conao3@gmail.com>
2026-02-22 21:47:50 +0900

    add xcitx5 config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -188,6 +188,62 @@
       ".config/wezterm/wezterm.lua".source = ./ext/wezterm.lua;
       ".config/zellij/config.kdl".source = ./ext/zellij__config.kdl;
       ".config/zellij/layouts/sando.kdl".source = ./ext/zellij__layouts__sando.kdl;
+      ".config/fcitx5/config".text = ''
+        [Hotkey]
+        EnumerateWithTriggerKeys=True
+        AltTriggerKeys=
+        EnumerateForwardKeys=
+        EnumerateBackwardKeys=
+        EnumerateSkipFirst=False
+        TogglePreedit=
+        ModifierOnlyKeyTimeout=250
+
+        [Hotkey/TriggerKeys]
+        0=Alt+space
+
+        [Hotkey/EnumerateGroupForwardKeys]
+        0=Super+space
+
+        [Hotkey/EnumerateGroupBackwardKeys]
+        0=Shift+Super+space
+
+        [Hotkey/ActivateKeys]
+        0=Hangul_Hanja
+
+        [Hotkey/DeactivateKeys]
+        0=Hangul_Romaja
+
+        [Hotkey/PrevPage]
+        0=Up
+
+        [Hotkey/NextPage]
+        0=Down
+
+        [Hotkey/PrevCandidate]
+        0=Shift+Tab
+
+        [Hotkey/NextCandidate]
+        0=Tab
+
+        [Behavior]
+        ActiveByDefault=False
+        resetStateWhenFocusIn=No
+        ShareInputState=All
+        PreeditEnabledByDefault=True
+        ShowInputMethodInformation=True
+        showInputMethodInformationWhenFocusIn=False
+        CompactInputMethodInformation=True
+        ShowFirstInputMethodInformation=True
+        DefaultPageSize=5
+        OverrideXkbOption=False
+        CustomXkbOption=
+        EnabledAddons=
+        DisabledAddons=
+        PreloadInputMethod=True
+        AllowInputMethodForPassword=False
+        ShowPreeditForPassword=False
+        AutoSavePeriod=30
+      '';
     };
 
     # https://search.nixos.org/packages?channel=24.11
