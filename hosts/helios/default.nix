ca826791cd55733f75933bb0f0abe5d9ee7060fc
conao3 <conao3@gmail.com>
2026-02-22 18:59:42 +0900

    memory issue

--- hosts/helios/default.nix
+++ hosts/helios/default.nix
@@ -13,4 +13,28 @@
   networking.hostName = "conao-nixos-helios";
 
   services.tailscale.enable = true;
+
+  zramSwap.enable = true;
+
+  systemd.user.services.memory-alert = {
+    description = "Memory usage alert";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.writeShellScript "memory-alert" ''
+        set -euxo pipefail -o posix
+        usage=$(${pkgs.gawk}/bin/awk '/MemTotal/ {total=$2} /MemAvailable/ {available=$2} END {printf "%d", (total - available) * 100 / total}' /proc/meminfo)
+        if [ "''${usage}" -ge 80 ]; then
+          ${pkgs.libnotify}/bin/notify-send -u critical "Memory Alert" "Memory usage: ''${usage}%"
+        fi
+      ''}";
+    };
+  };
+
+  systemd.user.timers.memory-alert = {
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      OnBootSec = "1min";
+      OnUnitActiveSec = "1min";
+    };
+  };
 }
