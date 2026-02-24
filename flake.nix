07378cb5ffcd4cfcffd90c35798463536ca5a5e1
conao3 <conao3@gmail.com>
2026-02-24 21:35:06 +0900

    specify nixpkgs

--- flake.nix
+++ flake.nix
@@ -22,6 +22,7 @@
     };
     llm-agents = {
       url = "github:numtide/llm-agents.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     cljgen = {
       url = "github:conao3/clojure-cljgen";
