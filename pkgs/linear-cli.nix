23390eed4e8861a37d463b50bc813d689ea59679
conao3 <conao3@gmail.com>
2026-02-24 15:45:52 +0900

    define linear-cli

--- pkgs/linear-cli.nix
+++ pkgs/linear-cli.nix
@@ -1,9 +1,41 @@
-{ writeShellApplication, deno }:
+{
+  lib,
+  stdenv,
+  fetchurl,
+  autoPatchelfHook,
+}:
+let
+  version = "1.10.0";
+  sources = {
+    x86_64-linux = {
+      url = "https://github.com/schpet/linear-cli/releases/download/v${version}/linear-x86_64-unknown-linux-gnu.tar.xz";
+      hash = "sha256-UZUYUkcHmh/cCM2xAxAeJrG1sdBj1fTB2n7HknjTdVg=";
+    };
+    aarch64-darwin = {
+      url = "https://github.com/schpet/linear-cli/releases/download/v${version}/linear-aarch64-apple-darwin.tar.xz";
+      hash = "sha256-gpxeAIKLgmc+UXTtFFME6pra5MElj7frWbGNSJQk7Ak=";
+    };
+  };
+in
+stdenv.mkDerivation {
+  pname = "linear-cli";
+  inherit version;
 
-writeShellApplication {
-  name = "linear";
-  runtimeInputs = [ deno ];
-  text = ''
-    exec ${deno}/bin/deno run -A --reload -q jsr:@schpet/linear-cli "$@"
+  src = fetchurl sources.${stdenv.hostPlatform.system};
+
+  nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
+
+  sourceRoot = ".";
+
+  installPhase = ''
+    install -Dm755 linear-*/linear $out/bin/linear
   '';
+
+  meta = {
+    description = "CLI tool for Linear";
+    homepage = "https://github.com/schpet/linear-cli";
+    license = lib.licenses.mit;
+    platforms = lib.attrNames sources;
+    mainProgram = "linear";
+  };
 }
