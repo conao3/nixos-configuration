80b37f5ca9695b6c3a804e124de04790f416181d
conao3 <conao3@gmail.com>
2026-02-26 11:44:48 +0900

    add dev server

--- pkgs/ports-portal-frontend/vite.config.js
+++ pkgs/ports-portal-frontend/vite.config.js
@@ -3,6 +3,16 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "127.0.0.1",
+    port: 5173,
+    proxy: {
+      "/data": {
+        target: "http://127.0.0.1:9500",
+        changeOrigin: false,
+      },
+    },
+  },
   build: {
     target: "es2020",
   },
