8c39909b9036a592f04e0c3296b007878eb8c34c
conao3 <conao3@gmail.com>
2026-02-26 11:52:10 +0900

    child flake

---
+++ pkgs/ports-portal-frontend/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "ports-portal-frontend development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs_24
+              pnpm
+            ];
+          };
+        }
+      );
+    };
+}
