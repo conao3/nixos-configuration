0174cafaa0fa0442addf11d71e264849c8ba78e6
conao3 <conao3@gmail.com>
2026-02-28 11:49:46 +0900

    add ghq cd subcommand via zsh wrapper function

--- home-manager/features/zsh.nix
+++ home-manager/features/zsh.nix
@@ -9,6 +9,17 @@
     initContent = ''
       [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
       bindkey -e
+      function ghq() {
+        if [ "$1" = "cd" ]; then
+          shift
+          local repo=$(command ghq list | fzf --query "$*")
+          if [ -n "$repo" ]; then
+            cd "$(command ghq root)/$repo"
+          fi
+        else
+          command ghq "$@"
+        fi
+      }
     '';
     envExtra = ''
       [[ -s "$HOME/.cargo/env" ]] && . "$HOME/.cargo/env"
