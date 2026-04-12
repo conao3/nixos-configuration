44f03a34083e6f917852ffdbde7aec27f248b686
conao3 <conao3@gmail.com>
2026-04-12 15:25:41 +0900

    update symphony revision

--- pkgs/symphony.nix
+++ pkgs/symphony.nix
@@ -12,15 +12,15 @@ let
   src = fetchFromGitHub {
     owner = "openai";
     repo = "symphony";
-    rev = "b0e0ff0082236a73c12a48483d0c6036fdd31fe1";
-    hash = "sha256-4gsuQmSX9/DC8PtXs448x1EU9EqOVjeHlEF9molpv+Q=";
+    rev = "9e89dd9ff0a3eddb8813c77f633ca4534d6e14b2";
+    hash = "sha256-0ViFsZlW4/3uH8AnuqLYlmwRu03HEGwfhB2NZIHFqY8=";
   };
 
   mixFodDeps = beamPkgs.fetchMixDeps {
     pname = "symphony-deps";
     version = "0.1.0";
     src = "${src}/elixir";
-    hash = "sha256-523u3y5kjnmbKLunK+yWJb+8BgPYy+STqfCjVi3zRy4=";
+    hash = "sha256-JdEnj95ol5raofHmyy18/bx+1akj/K3gxkxAnT1Lk2s=";
   };
 in
 beamPkgs.mixRelease {
