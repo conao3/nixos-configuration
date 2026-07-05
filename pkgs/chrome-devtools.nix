e43712e1f1334db7543b1b0c4f437a565ea166a5
conao3 <conao3@gmail.com>
2026-07-05 20:26:57 +0900

    pkgs/chrome-devtools: 0.6.3 -> 0.6.4

--- pkgs/chrome-devtools.nix
+++ pkgs/chrome-devtools.nix
@@ -15,13 +15,13 @@ in
 
 rustPlatform.buildRustPackage rec {
   pname = "chrome-devtools";
-  version = "0.6.3";
+  version = "0.6.4";
 
   src = fetchzip {
     name = "${pname}-${version}.tar.gz";
     url = "https://static.crates.io/crates/${pname}/${pname}-${version}.crate";
     extension = "tar.gz";
-    hash = "sha256-mpuuXnKuA385lpXPyktiO8EQUMWTO01oVzkp/I7lexI=";
+    hash = "sha256-OzzAo85sHg6rTnXanFIVl4kQiwS37r1Snt/1DXor7DM=";
   };
 
   cargoLock = {
