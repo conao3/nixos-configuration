1989fbcada4397fa537d379d63b6d0e6a9205551
conao3 <conao3@gmail.com>
2026-08-21 22:12:17 -0700

    home-manager: 1Password アプリを XFCE ログイン時に --silent で自動起動する

---
+++ home-manager/features/onepassword.nix
@@ -0,0 +1,9 @@
+{
+  xdg.configFile."autostart/1password.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=1Password
+    Exec=1password --silent
+    X-GNOME-Autostart-enabled=true
+  '';
+}
