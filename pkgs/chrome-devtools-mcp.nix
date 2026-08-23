cbd2fda9f7cdee1e1cbdad4f86f7ff24f93fff87
conao3 <4703128+conao3@users.noreply.github.com>
2026-08-23 21:10:56 +0000

    pkgs/chrome-devtools-mcp: 1.5.0 -> 1.7.0

--- pkgs/chrome-devtools-mcp.nix
+++ pkgs/chrome-devtools-mcp.nix
@@ -8,13 +8,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "chrome-devtools-mcp";
-  version = "1.5.0";
+  version = "1.7.0";
 
   src = fetchzip {
     name = "${pname}-${version}.tgz";
     url = "https://registry.npmjs.org/${pname}/-/${pname}-${version}.tgz";
     extension = "tar.gz";
-    hash = "sha256-c7DPr2CmBdAQeqakxp4lzCu/wZMuJdkfeoZqjfNmDp4=";
+    hash = "sha256-Jy4CIhpEAjxxHOsDC243ngJBnF5e40vXGmpbNgVWRc0=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
